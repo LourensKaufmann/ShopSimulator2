304e9bf01a4ff3f480ec629f00edd50df1b03ca1
RANDSTAD-VR-002\Randstad <l4.kauf@gmail.com>
2017-08-25 12:20:33 -0700

    Data scene controls changed

--- ShopSimulationModerator/Assets/Scripts/MenuHandler.cs
+++ ShopSimulationModerator/Assets/Scripts/MenuHandler.cs
@@ -10,6 +10,10 @@ public class MenuHandler : MonoBehaviour {
     Camera camera;
 
     [SerializeField]
+    private GameObject toggleH;
+    [SerializeField]
+    private GameObject toggleM;
+    [SerializeField]
     private GameObject canvas1;
     [SerializeField]
     private GameObject canvas2;
@@ -17,32 +21,46 @@ public class MenuHandler : MonoBehaviour {
     private GameObject matrix;
     [SerializeField]
     private GameObject shelf;
+    [SerializeField]
+    private GameObject heatMap;
 
     private void Start()
     {
-        canvas2.SetActive(false);   
+        canvas2.SetActive(false);
     }
 
     public void SwitchScreen()
     {
-        if (canvas1.active)
+        if (canvas1.activeSelf)
         {
             // Data is active so switch to Editor.
             canvas1.SetActive(false);
             matrix.SetActive(false);
             shelf.SetActive(false);
+            heatMap.SetActive(false);
             canvas2.SetActive(true);
         }
 
-        else if (canvas2.active)
+        else if (canvas2.activeSelf)
         {
             // Editor is active so switch to Data.
             canvas1.SetActive(true);
             matrix.SetActive(true);
             shelf.SetActive(true);
+            heatMap.SetActive(true);
             canvas2.SetActive(false);
         }
         
+    }
+
+    public void ToggleHeatmap ()
+    {
+        heatMap.SetActive(toggleH.GetComponent<Toggle>().isOn);
+    }
+
+    public void ToggleMatrix()
+    {
+        matrix.SetActive(toggleM.GetComponent<Toggle>().isOn);
     }
 
 }
